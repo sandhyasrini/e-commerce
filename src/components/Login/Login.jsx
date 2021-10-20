59f7804e36929a623e8bb290a61c52f7e4485a52
Sandhya Srinivasan <sandhya.srinivasan@lloydsbanking.com>
2021-10-20 22:05:42 +0530

    installed antd

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,22 +1,28 @@
 import React from "react";
 import  ReactDOM, {render} from "react-dom";
 import '../../assets/styles/login.less'
+import { Layout, Card } from "antd";
 
 export default function Login() {
     const login = {
         color: "blue"
         }
     return(
-        <div className="login">
+<Layout>
+    <Card>
             E-commerce
             <div className="login">
+                <div>
             username/e-mail
-            <input type="text"/>
+            <input type="text"/></div>
+            <div>
             password
-            <input type="password"/>
+            <input type="password"/></div>
+            <div>
             <button>Login</button>
-            <button>Signup</button>
+            <button>Signup</button></div>
             </div>
-        </div>
+            </Card>
+</Layout>
     ); 
 }
